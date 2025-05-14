7588273f66355fd8234404c4c12d764449c678cf
soumya <korlagani.soumya@gmail.com>
2025-05-14 20:27:04 +0530

    hello script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "please enter username::"
+#echo "please enter username::"
 
-read -s USERNAME #here USERNAME is variable
+#read -s USERNAME #here USERNAME is variable
 
-echo "please enter password::"
+#echo "please enter password::"
 
-read -s PASSWORD
+#read -s PASSWORD
+
+read -p "enter your username:"username
+read -p "enter your password:"password
 
 echo "Username is: $USERNAME, password is: $PASSWORD"
