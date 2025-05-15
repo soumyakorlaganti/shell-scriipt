0e51a16bc5aa564dc35fabca6991a67492c160ab
soumya <korlagani.soumya@gmail.com>
2025-05-15 15:29:17 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -23,8 +23,8 @@ else
     echo "You are super user."
 fi
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL"
 
-dnf install git -y
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL"
