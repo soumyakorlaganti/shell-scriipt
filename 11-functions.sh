7798fd03b01b8181bdeb774f85a8c5f0a7d1a734
soumya <korlagani.soumya@gmail.com>
2025-05-15 14:48:30 +0530

    functions

---
+++ 11-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"    
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql -y
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y
+VALIDATE $? "Installing MySQL"
